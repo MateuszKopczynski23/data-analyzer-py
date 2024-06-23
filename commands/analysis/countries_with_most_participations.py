a83cd5eff29568ccaafbaf55703a841857dc1d03
Mateusz Kopczyński <mateusz.kopczynski23@gmail.com>
2024-06-23 15:37:15 +0200

    Refactor command titles in commands

--- commands/analysis/countries_with_most_participations.py
+++ commands/analysis/countries_with_most_participations.py
@@ -6,7 +6,7 @@ class CountriesWithMostParticipations:
         num_results = int(input("Enter the number of results to display: "))
         most_participations_df = self.analyzer.countries_with_most_participations(num_results)
         if most_participations_df:
-            print("\nCountries with Most Participations:")
+            print("\nCountries with the most participations:")
             print(most_participations_df)
         else:
             print("Unable to find countries with most participations.")
--- commands/analysis/medals_by_country.py
+++ commands/analysis/medals_by_country.py
@@ -7,7 +7,7 @@ class MedalsByCountry:
         num_results = int(input("Enter the number of results to display: "))
         medals_by_country_df = self.analyzer.medals_by_country(medal, num_results)
         if medals_by_country_df:
-            print("\nMedals by Country:")
+            print("\nTop countries based on the specified medal type:")
             print(medals_by_country_df)
         else:
             print("No data available for the selected medal type.")
