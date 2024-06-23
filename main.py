5b29b5dded330ddfdafd0e02f362fa90a58082b5
Mateusz Kopczyński <mateusz.kopczynski23@gmail.com>
2024-06-23 14:16:34 +0200

    Add file loader

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 from data_manager import DataManager
 from analysis import DataAnalysis
+from loaders.file_loader import FileLoader
 from visualization import DataVisualization
-from resolvers.CommandResolver import CommandResolver
+from resolvers.command_resolver import CommandResolver
 
 
 def main():
-    # file_path = input("Enter the path to the CSV file: ")
-    file_path = 'data/olympic_committee_2022.csv'
+    file_path = input("Enter the path to the CSV file: ").strip()
+
+    file_loader = FileLoader(file_path)
+    file_loader.load_data()
+
+    data = file_loader.get_data()
+    if data is None:
+        print("Failed to load data. Exiting program.")
+        return
 
     data_manager = DataManager(file_path)
     analyzer = DataAnalysis(data_manager.data)
